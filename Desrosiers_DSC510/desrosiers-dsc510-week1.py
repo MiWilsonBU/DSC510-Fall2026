c7d49ea52d9574c172792741d0da776ad6bc1749
Robert Desrosiers <robert.drsj@gmail.com>
2026-09-12 16:03:21 -0500

    Create desrosiers-dsc510-week1.py
    Initial commit for Week 1 for Robert Desrosiers Jr.

---
+++ Desrosiers_DSC510/desrosiers-dsc510-week1.py
@@ -0,0 +1,18 @@
+# ============================================
+# DSC 510 Intro to Programming
+# Prof. Michael Eller
+# Week 1, Assignment 1.1 PyCharm
+# Author: Robert Desrosiers Jr (Repo by Michael Eller)
+# Date: 12 September 2026
+# Description: This program outputs "Hello world!".
+# ~~~~~
+# Change Iteration: 1
+# Change(s) Made: Initial commit for Week 1 for Robert Desrosiers Jr.
+# Date of Change: 12 September 2026
+# Modifier: Robert Desrosiers Jr
+# Approved By: Robert Desrosiers Jr
+# Date Moved to Production: 12 September 2026
+# ===========================================
+
+# Simply print Hello World without complicating the script with unnecessary functions.
+print('Hello world!')
