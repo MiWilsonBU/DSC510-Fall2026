749e34bb8e9ec56ef312676f4d2a9e1b2335db40
Andrew Bach <328517442+andrewbachcodes@users.noreply.github.com>
2026-09-15 00:14:01 -0500

    Improvements to make week 2 assignment more readable by moving logic into functions

--- Bach_DSC510/abach_2.1 assignment.py
+++ Bach_DSC510/abach_2.1 assignment.py
@@ -4,6 +4,13 @@ Author: Andrew Bach
 Date: 9/14/2026
 
 Program used to calculate the cost of an order of fiber optic cable
+
+Change Log:
+    Change: 1
+    Changes made: Moved logic into methods to improve readability
+    Date of change: 9/14/2026
+    Author: Andrew Bach
+    Change Approved by: Andrew Bach
 """
 import locale
 locale.setlocale(locale.LC_ALL, 'C')
@@ -16,21 +23,49 @@ def format_currency(amount):
     return '${:,.2f}'.format(amount)
 
 
+def get_company():
+    company_name = ""
+
+    while not company_name:
+        company_name = input("\nWhat company are you ordering for: ").strip()
+
+        if not company_name:
+            print("Please enter a company name.")
+
+    return company_name
+
+
+def get_order_quantity():
+    quantity = 0
+
+    while quantity <= 0:
+        try:
+            quantity = int(input("\nHow many feet of fiber optic cable would you like to order: "))
+
+            if quantity <= 0:
+                print("Please enter a number greater than 0 for your order.")
+                continue
+
+            total = quantity * COST_PER_FOOT
+
+            print(f"\n{quantity} feet of fiber optic cable will cost a total of {format_currency(total)}.")
+            user_input = input("Would you like to proceed? (Y/N): ").upper()
+
+            if user_input in ("N", "NO", "NOPE"):
+                continue
+
+        except ValueError:
+            print("An invalid value was entered, please enter a numeric value for your order.")
+
+    return quantity, total
+
+
 def main():
     continue_ordering = True
     while continue_ordering:
-        # Order values
-        company = ""
-        order_quantity = 0
-        order_total = 0
-
         print("Welcome to the Bach Fiber Optics. Lets get ready to place your order!")
 
-        while not company:
-            company = input("\nWhat company are you ordering for: ").strip()
-
-            if not company:
-                print("Please enter a company name.")
+        company = get_company()
 
         print(
             f"\nWe are looking forward to helping {company} with their Fiber Optic Cable Order."
@@ -38,32 +73,13 @@ def main():
             "\nFiber Optic Cable | $0.95 per foot"
         )
 
-        while order_quantity <= 0:
-            try:
-                order_quantity = int(input("\nHow many feet of fiber optic cable would you like to order: "))
+        order_quantity, order_total = get_order_quantity()
 
-                if order_quantity <= 0:
-                    print("Please enter a number greater than 0 for your order.")
-                    continue
-
-                order_total = order_quantity * COST_PER_FOOT
-
-                print(f"\n{order_quantity} feet of fiber optic cable will cost a total of {format_currency(order_total)}.")
-                user_input = input("Would you like to proceed? (Y/N): ").upper()
-
-                if user_input in ("N", "NO", "NOPE"):
-                    order_quantity = 0
-
-            except ValueError:
-                print("An invalid value was entered, please enter a numeric value for your order.")
-
+        # Print Receipt
         print(
             "\nYour order has been placed!"
             "\nHere is your receipt:"
-        )
-
-        print(
-            "-------------------------------------------"
+            "\n-------------------------------------------"
             "\nBach Fiber Optics"
             "\n-------------------------------------------"
             f"\nSales Receipt for {company}"
