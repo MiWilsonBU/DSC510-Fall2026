c8f51bcae48b1e6dacb4a12aa5cc04118207107a
Andrew Bach <328517442+andrewbachcodes@users.noreply.github.com>
2026-09-14 23:49:20 -0500

    Add week 2 assignment

---
+++ Bach_DSC510/abach_2.1 assignment.py
@@ -0,0 +1,83 @@
+"""
+DSC 510 - Week 2 - 2.1 Assignment
+Author: Andrew Bach
+Date: 9/14/2026
+
+Program used to calculate the cost of an order of fiber optic cable
+"""
+import locale
+locale.setlocale(locale.LC_ALL, 'C')
+
+# Cost per foot of fiber optic cable
+COST_PER_FOOT = 0.95
+
+
+def format_currency(amount):
+    return '${:,.2f}'.format(amount)
+
+
+def main():
+    continue_ordering = True
+    while continue_ordering:
+        # Order values
+        company = ""
+        order_quantity = 0
+        order_total = 0
+
+        print("Welcome to the Bach Fiber Optics. Lets get ready to place your order!")
+
+        while not company:
+            company = input("\nWhat company are you ordering for: ").strip()
+
+            if not company:
+                print("Please enter a company name.")
+
+        print(
+            f"\nWe are looking forward to helping {company} with their Fiber Optic Cable Order."
+            "\n-Current Costs-"
+            "\nFiber Optic Cable | $0.95 per foot"
+        )
+
+        while order_quantity <= 0:
+            try:
+                order_quantity = int(input("\nHow many feet of fiber optic cable would you like to order: "))
+
+                if order_quantity <= 0:
+                    print("Please enter a number greater than 0 for your order.")
+                    continue
+
+                order_total = order_quantity * COST_PER_FOOT
+
+                print(f"\n{order_quantity} feet of fiber optic cable will cost a total of {format_currency(order_total)}.")
+                user_input = input("Would you like to proceed? (Y/N): ").upper()
+
+                if user_input in ("N", "NO", "NOPE"):
+                    order_quantity = 0
+
+            except ValueError:
+                print("An invalid value was entered, please enter a numeric value for your order.")
+
+        print(
+            "\nYour order has been placed!"
+            "\nHere is your receipt:"
+        )
+
+        print(
+            "-------------------------------------------"
+            "\nBach Fiber Optics"
+            "\n-------------------------------------------"
+            f"\nSales Receipt for {company}"
+            "\n-------------------------------------------"
+            "\nQty     | Product                | Price"
+            f"\n{order_quantity}      | Fiber optic cable      | {format_currency(order_total)}\n"
+            f"\nTotal: {format_currency(order_total)}"
+            "\n-------------------------------------------"
+        )
+
+        user_input = input("\nWould you like to place another order? (Y/N): ").upper()
+
+        if user_input in ("N", "NO", "NOPE"):
+            continue_ordering = False
+
+if __name__ == "__main__":
+    main()
