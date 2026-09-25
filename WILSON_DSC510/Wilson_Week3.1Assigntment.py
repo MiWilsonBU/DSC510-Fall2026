8af1a811e6502629dd10f24e9ff3a1b87169ff7c
MiWilsonBU <michaewilson@my365.bellevue.edu>
2026-09-25 15:26:31 -0500

    Version 1.Ooops

--- WILSON_DSC510/Wilson_Week3.1Assigntment.py
+++ WILSON_DSC510/Wilson_Week3.1Assigntment.py
@@ -9,9 +9,13 @@ retrieve the # of feet of fiber optic cable, Calculate and produce an invoice ba
 
 print("Welcome to the Mike's House of Fiber Optic Cable!")  #we are welcoming the user as we are nice people and appreciate their business
 Comp_Name = input("What is your company's name? ")  #Asking for the customers name for the invoice header
-FOC_FT = float(input("What length of Fiber Optic Cable, in feet, is being installed? "))  #asking for length of cable and labeling it float for calculation purposes
-FOC_CostperFT = 0.95 # current price per foot for cabling
-FOC_TotCost = FOC_FT * FOC_CostperFT  #calculating the total price for the invoice
+try:
+    FOC_FT = float(input("What length of Fiber Optic Cable, in feet, is being installed? "))  #asking for length of cable and labeling it float for calculation purposes
+    FOC_CostperFT = 0.95 # current price per foot for cabling
+    FOC_TotCost = FOC_FT * FOC_CostperFT  #calculating the total price for the invoice
+
+except:
+    print("Please enter a numeric value")
 
 print("The follow is a quote for our favorite customer: ", Comp_Name)  #invoice header is name entered
 print("The total length of Fiber Optic Cable to be installed is ", f"{FOC_FT:,.2f}"," Feet")  #telling the customer how much product we are costing
